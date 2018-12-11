99fb6c0a5d941afd2982de9a993e664d9f6d84fc
Madhav <akshaybk15@gmail.com>
2018-12-11 15:29:28 +0530

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,9 +8,9 @@ import { User } from '../user';
 })
 export class UserService {
 
-  // private baseUrl : string = 'http://ec2-18-220-231-127.us-east-2.compute.amazonaws.com:9090/api';
+  // private baseUrl : string = 'http://ec2-3-17-27-93.us-east-2.compute.amazonaws.com:9090/rest';
 
-  private baseUrl : string = 'http://localhost:9090/rest';
+  // private baseUrl : string = 'http://localhost:9090/rest';
 
   private headers = new HttpHeaders({"Content_Type":"application/json"});
 
