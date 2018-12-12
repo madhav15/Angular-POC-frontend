61e35c1000dd791ed57f77c713520df952e7ff7b
Madhav <akshaybk15@gmail.com>
2018-12-12 12:32:58 +0530

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { User } from '../user';
 })
 export class UserService {
 
-  private baseUrl : string = 'http://ec2-3-17-27-93.us-east-2.compute.amazonaws.com:9090/rest';
+  private baseUrl : string = 'http://ec2-3-17-27-93.us-east-2.compute.amazonaws.com:9090/webapps/rest';
 
   // private baseUrl : string = 'http://localhost:9090/rest';
 
